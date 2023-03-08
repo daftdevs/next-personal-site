9101457b5aaf9e7550a44551219bd01843e21d85
Daft <daftdevs@gmail.com>
2023-03-08 23:38:16 +0700

    feat: load satoshi using localFont

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const satoshi = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Variable.ttf",
+      weight: "300 900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-VariableItalic.ttf",
+      weight: "300 900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-satoshi",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${satoshi.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
