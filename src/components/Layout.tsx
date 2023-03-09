a9f83ea021e351ffbefd153cc90119262d5c0058
Daft <daftdevs@gmail.com>
2023-03-09 22:19:27 +0700

    feat: init layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import React, { HTMLAttributes } from "react";
+
+type LayoutProps = {
+  title?: string;
+  description?: string;
+  className?: string;
+  children: React.ReactNode;
+};
+
+const Layout = ({ title, description, className, children }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className={`min-h-screen w-screen bg-gray-200 ${className}`}>
+          <div className="mx-8 lg:mx-auto max-w-7xl">{children}</div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Layout;
