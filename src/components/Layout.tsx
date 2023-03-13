5628ece7665831adade69147812c31fd41df8521
Daft <daftdevs@gmail.com>
2023-03-13 22:22:25 +0700

    ref: change default bg and text color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,7 +18,7 @@ const Layout = ({ title, description, className, children }: LayoutProps) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className={`min-h-screen w-screen bg-gray-200`}>
+        <div className={`min-h-screen w-screen bg-neutral-900 text-white`}>
           <div className={`mx-8 lg:mx-auto max-w-7xl ${className}`}>
             {children}
           </div>
