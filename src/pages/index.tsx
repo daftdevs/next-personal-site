6e1690e831ca45c0233e9b180ebcbda0beb90b8d
Daft <daftdevs@gmail.com>
2023-03-14 23:45:46 +0700

    ref: intro header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 import Layout from "@/components/Layout";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
     <Layout title="Home">
       <div className="font-normal pt-12">
-        <h1 className="text-6xl font-bold">Hello, world-</h1>
-        <h3 className="text-2xl pt-4">
-          I&#39;m Daffa, a Software Engineer from Indonesia (🇮🇩)
-        </h3>
-        <h3 className="text-2xl">
-          Been crafting the Frontend side of the web using
-          Javascript/Typescript.
-        </h3>
+        <div className="flex flex-row justify-between items-center">
+          <h1 className="text-7xl font-bold">Hello, world-</h1>
+          <div className="flex-row gap-4 hidden lg:flex">
+            <LinkedInLogoIcon
+              width={25}
+              height={25}
+              className="hover:text-blue-400 cursor-pointer"
+            />
+            <GitHubLogoIcon
+              width={25}
+              height={25}
+              className="hover:text-blue-400 cursor-pointer"
+            />
+          </div>
+        </div>
       </div>
     </Layout>
   );
